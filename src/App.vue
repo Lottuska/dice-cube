a44e0deab756eea569d7dcbb6e66c7f82c917350
N3093 <N3093@student.jamk.fi>
2021-12-13 18:06:54 +0200

    Dice is working

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1>Roll the dice!</h1>
+    <Dice />
+  </div>
+</template>
+
+<script>
+import Dice from './components/Dice.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dice
+  }
+}
+</script>
+
+<style>
+#app {
+  background-color: #41b883 !important;
+  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+h1 {
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 2.6em;
+  margin: 0;
+  padding: 0;
+  text-shadow: 2px 2px 0px #000000,
+              2px 2px 5px #2b7a57;
+}
+</style>
---
+++ src/components/Dice.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+    <div class="cube">
+      <img v-if="number !== null" :src="numberImage" :alt="'Dice value is currently ' + number" draggable="false" />
+    </div>
+    <button class="button" v-on:click="randomNumber">Roll</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dice",
+  data() {
+    return {
+      number: null,
+      numberImage: null,
+    };
+  },
+  methods: {
+    randomNumber: function () {
+      this.number = Math.floor(Math.random() * 6 + 1); // Random number between 1 to 6
+      switch (this.number) {
+        case 1:
+          this.numberImage = "images/1.png";
+          break;
+        case 2:
+          this.numberImage = "images/2.png";
+          break;
+        case 3:
+          this.numberImage = "images/3.png";
+          break;
+        case 4:
+          this.numberImage = "images/4.png";
+          break;
+        case 5:
+          this.numberImage = "images/5.png";
+          break;
+        case 6:
+          this.numberImage = "images/6.png";
+          break;
+        default:
+          this.numberImage = "";
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.cube {
+  margin: 100px 0px;
+  height: 80px;
+  width: 80px;
+  border-radius: 8px;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 2px 7px 7px #2b7a57;
+}
+.cube img {
+  width: 70px;
+}
+.button {
+  padding: 20px;
+  width: 220px;
+  background-color: #ffffff;
+  border: none;
+  color: #41b883;
+  box-shadow: 2px 7px 7px #2b7a57;
+  font-weight: bold;
+}
+.button:hover {
+  box-shadow: 2px 4px 4px #2b7a57;
+  cursor: pointer;
+}
+</style>
